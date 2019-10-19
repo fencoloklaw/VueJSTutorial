f1a14c79c7af7f076155f4370f857ec3b3db5dcd
Fenco <fencoloklaw@gmail.com>
2019-10-19 15:15:43 -0700

    data binding example

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -2,17 +2,41 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Hello World!"/>
+    <employee-table v-bind:employees="employees" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import EmployeeTable from './components/EmployeeTable.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    EmployeeTable,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
+  },
 }
 </script>
 
