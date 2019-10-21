775d36375ac6464f6b839825b86a2446caf526dc
fencoloklaw <fencoloklaw@gmail.com>
2019-10-21 01:59:44 -0700

    CRUD operations, updates, employee form

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,20 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hello World!"/>
-    <employee-table v-bind:employees="employees" />
-  </div>
+	<div id="app" class="small-container">
+		<h1>Employees</h1>
+		<!-- <HelloWorld msg="Hello World!"/> -->
+		<employee-form @add:employee="addEmployee"/>
+		<employee-table :employees="employees" 
+						@delete:employee="deleteEmployee" 
+						@edit:employee="editEmployee"
+		/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 import EmployeeTable from './components/EmployeeTable.vue';
+import EmployeeForm from './components/EmployeeForm.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    // HelloWorld,
     EmployeeTable,
+    EmployeeForm,
   },
   data() {
     return {
@@ -37,16 +43,38 @@ export default {
       ],
     }
   },
+  methods: {
+		addEmployee(employee) {
+			const lastId =
+				this.employees.length > 0
+				? this.employees[this.employees.length - 1].id
+				: 0;
+			const id = lastId + 1;
+			const newEmployee = { ...employee, id };
+
+			this.employees = [...this.employees, newEmployee];
+		},
+		deleteEmployee(id) {
+			this.employees = this.employees.filter(
+			employee => employee.id !== id
+			)
+		},
+		editEmployee(id, updatedEmployee) {
+			this.employees = this.employees.map(employee =>
+				employee.id === id ? updatedEmployee : employee
+			)
+		}
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	button {
+		background: #009435;
+		border: 1px solid #009435;
+	}
+
+	.small-container {
+		max-width: 680px;
+	}
 </style>
